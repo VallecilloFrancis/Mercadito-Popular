ef0f4412366158976e055fa6569f5a30d7deda66
VallecilloFrancis <89234319+gabbysolorzano@users.noreply.github.com>
2021-08-20 11:14:12 -0600

    Aporte de Gabby

---
+++ database/migrations/2021_08_19_190240_create_detalles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetallesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('facturaid');
+            $table->unsignedBigInteger('productoid');
+            $table->integer('cantidad');
+            $table->foreign('productoid')->references('id')->on('productos')->onDelete('cascade');
+            $table->foreign('facturaid')->references('id')->on('facturas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalles');
+    }
+}
