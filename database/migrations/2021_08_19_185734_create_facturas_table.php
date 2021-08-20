f2e5b2ed2b28d9557ae4b8c6e55e4d088e306f2a
VallecilloFrancis <89233276+VallecilloFrancis@users.noreply.github.com>
2021-08-20 11:11:06 -0600

    Aporte Francis

---
+++ database/migrations/2021_08_19_185734_create_facturas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacturasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facturas', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha_venta');
+            $table->unsignedBigInteger('clienteid');
+            $table->foreign('clienteid')->references('id')->on('clientes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facturas');
+    }
+}
